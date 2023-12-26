93ff8f389ae803e6a4e0aec9ad320cbbf04f429e
kchyun <judy8823@korea.ac.kr>
2023-12-26 23:16:30 +0900

    Mod: style classifier

--- style_aware_net/model/style_classifier.py
+++ style_aware_net/model/style_classifier.py
@@ -16,11 +16,13 @@ class StyleClassifier():
     
     @torch.no_grad()
     def forward(self, anc, pos, device):
-        pos_similarity = pairwise_cosine_similarity(pos, self.prompt_embeddings.squeeze(1).to(device))
-        anc_similarity = pairwise_cosine_similarity(anc, self.prompt_embeddings.squeeze(1).to(device))
+        pos_similarity = pairwise_cosine_similarity(pos.to(device), self.prompt_embeddings.squeeze(1).to(device))
+        anc_similarity = pairwise_cosine_similarity(anc.to(device), self.prompt_embeddings.squeeze(1).to(device))
 
-        logits = torch.min(torch.stack([anc_similarity, pos_similarity]), 0).values
-        logits = torch.nn.functional.softmax(logits / self.temperature, dim=1).detach() # 차이를 좀 두드러지게하고싶어서, Temparature scaling했습니다.
+        logits = (pos_similarity + anc_similarity)/2
+        
+        # logits = torch.min(torch.stack([anc_similarity, pos_similarity]), 0).values
+        # logits = torch.nn.functional.softmax(logits / self.temperature, dim=1).detach()
         return logits
 
     def idx2stl(self, idx):
