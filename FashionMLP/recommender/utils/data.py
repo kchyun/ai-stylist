72b8113554e3a0b43e3b7125291d80a84d301d63
kchyun <judy8823@korea.ac.kr>
2023-12-27 21:03:29 +0900

    Change FashionMLP directory

--- FashionMLP/recommender/utils/data.py
+++ FashionMLP/recommender/utils/data.py
@@ -13,10 +13,15 @@ from typing import Optional, Dict, List, Set, Tuple
 
 from copy import deepcopy
 
-PAIRS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/gnd_top_bottom_pairs.csv"
-TOP_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/top_embeds.json"
-BOTTOM_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/bottom_embeds.json"
+# FashionVC Dataset
+# PAIRS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/gnd_top_bottom_pairs.csv"
+# TOP_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/top_embeds.json"
+# BOTTOM_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/datasets/FashionVCdata/FashionVCdata/bottom_embeds.json"
 
+# Polyvore
+PAIRS_PATH = "C:/KU/ai-stylist/ai-stylist/data/polyvore_cleaned/train.json"
+TOP_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/data/polyvore_cleaned/top_embeds.json"
+BOTTOM_EMBEDS_PATH = "C:/KU/ai-stylist/ai-stylist/data/polyvore_cleaned/bottom_embeds.json"
 class FashionMLPDataset(Dataset):
     def __init__(
         self,
@@ -63,9 +68,57 @@ class FashionMLPDataset(Dataset):
             items = json.load(f)
         return {str(item['id']): Tensor(item['embed']) for item in items}
 
+class PolyvoreDataset(Dataset):
+    def __init__(
+        self,
+        pairs: Dict[str, Set[str]],
+        dataset_type: str='train'
+        ):
+        super(PolyvoreDataset).__init__()
+        self.dataset_type = dataset_type
+        self.pairs = pairs
+        self.data = [(top, bottom) for top in self.pairs.keys() for bottom in self.pairs.get(top)]
+        self.top_embeds = self._preprocess_embeds(TOP_EMBEDS_PATH)
+        self.bottom_embeds = self._preprocess_embeds(BOTTOM_EMBEDS_PATH)
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        if self.dataset_type == 'train':
+            top_idx, bottom_idx = self.data[idx]
+            neg_bottom_idxs = self._get_neg_bottom(top_idx, n=128)
+
+            source_embed = self.top_embeds[top_idx]
+            pos_embed = self.bottom_embeds[bottom_idx]
+            neg_embeds = [self.bottom_embeds[neg_bottom_idx] for neg_bottom_idx in neg_bottom_idxs]
+
+            item = (source_embed, pos_embed, neg_embeds)
+        else:
+            top_idx, bottom_idx = self.data[idx]
+            item = {'input_embed' : self.top_embeds[top_idx],
+                    'target' : bottom_idx}
+            
+        return item
+            
+    def _get_neg_bottom(self, top_idx, n):
+        neg_bottom_idxs = []
+        while len(neg_bottom_idxs) < 128:
+            neg_bottom_idx = self.data[random.randint(0, len(self.data) - 1)][-1]
+            if neg_bottom_idx not in self.pairs[top_idx]:
+                neg_bottom_idxs.append(str(neg_bottom_idx))
+        return neg_bottom_idxs
+        
+    def _preprocess_embeds(self, path):
+        with open(path, 'r') as f:
+            items = json.load(f)
+        return {str(item['id']): Tensor(item['embed']) for item in items}
+
+
 
 def get_dataset(model_type='MLP') -> Tuple[FashionMLPDataset, FashionMLPDataset]:
     print('Dataset generating starts...')
+    df = json.load()
     pair_df = pd.read_csv(PAIRS_PATH)
     pairs = defaultdict(list)
     for i, (top, bottom) in pair_df.iterrows():
